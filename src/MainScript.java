00b677fbe828bd36577201b945beca62e23087ad
davidangulo-sketch <david.angulo@est.iudigital.edu.co>
2026-03-08 19:55:22 -0500

    java.util.Scanner; //Importación de libreria para inputs

--- src/MainScript.java
+++ src/MainScript.java
@@ -1,5 +1,5 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner; //Importación de libreria para inputs
+
 public class MainScript {
     public static void main(String[] args) {
         //En este archivo estará toda la lógica de la activada
